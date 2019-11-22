5da9b8aec3654d7e61e621aabf976109a981c55e
Adam Obuchowicz <adam.obuchowicz@luna-lang.org>
2019-11-22 10:50:51 +0100

    Asynchronous compilation support + fixes
    Chrome does not allows synchronous WASM compilation of > 4KB files,
    thus we turned on BINARYEN_ASYNC_COMPILATION emscripten flag.
    There were also two bugs:
    - Bug: Because FT_NewFaceMemory function requires that data will be
    not freed until destroying the face, we need to copy data we've
    received and keep in with font handle
    - Bug: Loading msdfgen_wasm.js in web_pack requires function to be
    exported

---
+++ msdfgen/wasm/post.js
@@ -0,0 +1,11 @@
+Module.isInitialized = isInitialized
+Module.addInitializationCb = addInitializationCb
+
+exports.ccall=ccall
+exports.getValue=getValue
+exports._msdfgen_maxMSDFSize=_msdfgen_maxMSDFSize
+exports._msdfgen_generateMSDF=_msdfgen_generateMSDF
+exports._msdfgen_freeFont=_msdfgen_freeFont
+exports.addInitializationCb=addInitializationCb
+exports.isInitialized=isInitialized
+
